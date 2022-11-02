034959bd28ccc9568a328f56994f6f7ae589cc34
DIVINE <DIVINE@MacBook-Air-2.hsd1.tn.comcast.net>
2022-11-02 07:57:48 -0500

    authorization start

--- seeds/index.js
+++ seeds/index.js
@@ -39,6 +39,7 @@ const seedDB = async ()=>{
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground ({
+            author: "6358ac167b258feffc6d4e66",
             imageUrl: await seedImg(),
             title : `${sample(descriptors)}, ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
